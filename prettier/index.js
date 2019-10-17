5edf4e5fb7e17967d0640642487675a63b7d46a1
hwaterke <hwaterke@users.noreply.github.com>
2019-10-17 21:40:07 +0200

    Initial test with prettier

---
+++ prettier/index.js
@@ -0,0 +1,27 @@
+const {json, packageJson, install} = require('mrm-core')
+
+const PRETTIER_PATTERN = '**/*.{js,jsx,ts,tsx,css,scss,json,md,yml}'
+
+const PRETTIER_CONFIG = {
+  endOfLine: 'lf',
+  bracketSpacing: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+}
+
+function task() {
+  json('.prettierrc.json')
+    .set(PRETTIER_CONFIG)
+    .save()
+
+  packageJson()
+    .setScript('format', `prettier --write "${PRETTIER_PATTERN}"`)
+    .save()
+
+  // Dependencies
+  install(['prettier'], {dev: true})
+}
+
+task.description = 'Adds Prettier'
+module.exports = task
